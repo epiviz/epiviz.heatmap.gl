d5f2878a019b630e073672fd19b3ec7452ac3d61
jkanche <jayaram.kancherla@gmail.com>
2023-02-08 05:37:22 +0000

    Deploying to gh-pages from @ jkanche/epiviz.heatmap.gl@70a9f416ebe9678358ff2be8150863c83d64a32f 🚀

--- data.js
+++ data.js
@@ -1,11 +1,38 @@
+let nrows = 10,
+  ncols = 15;
+
+let x = [],
+  y = [],
+  xlabels = [],
+  ylabels = [],
+  var_intensity = [],
+  var_size = [];
+
+for (let ilx = 0; ilx < nrows; ilx++) {
+  xlabels.push(`r${ilx}`);
+  for (let ily = 0; ily < ncols; ily++) {
+    x.push(ilx);
+    y.push(ily);
+    var_intensity.push(Math.random());
+    var_size.push(25 * Math.random());
+  }
+}
+
+for (let ily = 0; ily < ncols; ily++) {
+  ylabels.push(`c${ily}`);
+}
+
+// map intensities to color
+let color_map = var_intensity.map((x, i) => d3.interpolateViridis(x))
+
 const data = {
-  x: [0, 0, 0, 1, 1, 1],
-  y: [0, 1, 2, 0, 1, 2],
-  xlabels: ["ax", "bx"],
-  ylabels: ["ay", "by", "cy"],
+  x: x,
+  y: y,
+  xlabels: xlabels,
+  ylabels: ylabels,
 };
 
-const color = [16357938, 16357938, 2418580, 2418580, 2418580, 16357938];
-const size = [5, 20, 25, 25, 20, 15];
+const color = color_map;
+const size = var_size;
 
 export { data, color, size };
