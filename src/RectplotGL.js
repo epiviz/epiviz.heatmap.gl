e984c1487451e3315f2792dc6c450947c985afa1
Ossama Rafique <contact@ossamarafique.com>
2023-06-21 11:13:42 -0700

    Improved label rendering logic, support truncation (#15)
    * Improved Labels rendering logic to support truncation
    * Bump up library versions

--- src/RectplotGL.js
+++ src/RectplotGL.js
@@ -62,44 +62,6 @@ class RectplotGL extends BaseGL {
     spec_inputs.width = this.input.x.map((e, i) => default_width - xGaps(i));
     spec_inputs.height = this.input.y.map((e, i) => default_height - yGaps(i));
 
-    // config for labels
-    let labels = null;
-    if ("xlabels" in this.input && this.input["xlabels"] !== null) {
-      labels = [];
-
-      const xlabels_len = this.input["xlabels"].length;
-      for (let ilx = 0; ilx < xlabels_len; ilx++) {
-        labels.push({
-          x: -1.05 + (2 * ilx + 1) / xlabels_len,
-          y: 1.05,
-          type: "row",
-          index: ilx,
-          text: this.input["xlabels"][ilx],
-          fixedY: true,
-          "text-anchor": "center",
-        });
-      }
-    }
-
-    if ("ylabels" in this.input && this.input["ylabels"] !== null) {
-      if (labels == null) {
-        labels = [];
-      }
-
-      const ylabels_len = this.input["ylabels"].length;
-      for (let ily = 0; ily < ylabels_len; ily++) {
-        labels.push({
-          x: -1.05,
-          y: -1.05 + (2 * ily + 1) / ylabels_len,
-          type: "column",
-          index: ily,
-          text: this.input["ylabels"][ily],
-          fixedX: true,
-          "text-anchor": "end",
-        });
-      }
-    }
-
     let spec = {
       margins: {
         top: "25px",
@@ -135,10 +97,7 @@ class RectplotGL extends BaseGL {
       ],
     };
 
-    if (labels !== null) {
-      spec["labels"] = labels;
-    }
-
+    this._generateSpecForLabels(spec);
     this._generateSpecForEncoding(spec, "color", this.state.color);
     this._generateSpecForEncoding(spec, "size", this.state.size);
     this._generateSpecForEncoding(spec, "opacity", this.state.opacity);
