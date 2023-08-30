c253df1e6b98b100d86910cf00ced84f02935745
OssamaRafique <contact@ossamarafique.com>
2023-08-30 19:27:49 -0400

    Fixed Dotplot Sizing & Size Legend Issue

--- src/DotplotGL.js
+++ src/DotplotGL.js
@@ -231,7 +231,8 @@ class DotplotGL extends BaseGL {
    */
   renderSizeLegend() {
     if (!this.sizeLegendData) return;
-    let { minSize, maxSize, steps, maxSizeInPx } = this.sizeLegendData;
+    let { minSize, maxSize, steps, maxSizeInPx, minSizeInPx } =
+      this.sizeLegendData;
     const [, maxX] = getMinMax(this.input.x);
     const [, maxY] = getMinMax(this.input.y);
     let xlen = maxX + 1,
@@ -261,8 +262,8 @@ class DotplotGL extends BaseGL {
     // Desired max size in pixels
     const maxPx = maxSizeInPx || maxSize;
 
-    // Calculate the desired minimum size in pixels proportionally
-    const minPx = (minSize * maxPx) / maxSize;
+    // Desired max size in pixels
+    const minPx = minSizeInPx || minSize;
 
     // Create a linear scale
     const sizeScale = scaleLinear()
